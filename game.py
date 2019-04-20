a2072759aea4038d0107a4e234f1fd4117db8a61
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-20 12:51:54 -0700

    Greeting the Player

---
+++ game.py
@@ -0,0 +1,25 @@
+
+print ("Welcome to the Game")
+name = input("What is your name? >")
+import random
+random_number = random.randint(1, 100)
+print (random_number)
+count = 1
+
+user_number = input("Tell me a number from 1 to 100 >")
+while int(user_number) != int(random_number):
+	user_number = input("Try again >")
+
+	if int(user_number) > int(random_number):
+		print ("too hight")
+	elif int(user_number) < int(random_number):
+		print ("too low")
+	count += 1
+	
+print ("yes! " + name + " you are right! and it only took you " + str(count) + " times")
+
+
+
+
+
+
